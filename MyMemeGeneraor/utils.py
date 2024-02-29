ab9df080479143aa4e26c96122ba312ce1457ff8
ORATILE  MOKGOATLHENG <125035665+OratileMokgoatlheng@users.noreply.github.com>
2024-02-29 23:09:37 +0200

    new condition for the util

--- MyMemeGeneraor/utils.py
+++ MyMemeGeneraor/utils.py
@@ -11,3 +11,25 @@ def load_font(font_path, font_size):
 def add_text(draw, text, font, position, max_width):
     # Calculate text dimensions
     text_width, text_height = draw.textsize(text, font=font)
+
+     Check if text exceeds max width
+    if text_width > max_width:
+        # If text exceeds max width, wrap it
+        lines = []
+        line = ''
+        for word in text.split():
+            if draw.textsize(line + word, font=font)[0] <= max_width:
+                line += word + ' '
+            else:
+                lines.append(line)
+                line = word + ' '
+        lines.append(line)
+
+        # Draw wrapped text
+        y = position[1]
+        for line in lines:
+            draw.text((position[0], y), line, font=font, fill="white", stroke_width=2, stroke_fill="black")
+            y += text_height
+    else:
+        # If text fits within max width, draw normally
+        draw.text(position, text, font=font, fill="white", stroke_width=2, stroke_fill="black")

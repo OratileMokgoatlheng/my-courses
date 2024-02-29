ef3f447b731d8b1c4fbb809d19c0fdb5d3d33ad7
ORATILE  MOKGOATLHENG <125035665+OratileMokgoatlheng@users.noreply.github.com>
2024-02-29 23:03:01 +0200

    calculating the size and the position

--- MyMemeGeneraor/meme_generator.py
+++ MyMemeGeneraor/meme_generator.py
@@ -14,3 +14,13 @@ def generate_meme(image_path, top_text, bottom_text, font_path="arial.ttf", font
     except IOError:
         font = ImageFont.load_default()
         print("Font not found. Using default font.")
+
+
+        # Calculate text size and position
+    text_width, text_height = draw.textsize(top_text, font=font)
+    x_top = (image_width - text_width) / 2
+    y_top = 10
+
+    text_width, text_height = draw.textsize(bottom_text, font=font)
+    x_bottom = (image_width - text_width) / 2
+    y_bottom = image_height - text_height - 10
